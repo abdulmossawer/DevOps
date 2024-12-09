1d3beb914cc8fc4d1d93d2c17fabd2a522e81801
Abdul Mossawer <142085179+abdulmossawer@users.noreply.github.com>
2024-12-09 13:48:46 +0530

    Commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from 'express'; // Use import syntax
+
 const app = express();
 const port = 3000;
 
---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai'; // ES module import
+
+describe('Basic Arithmetic Tests', () => {
+  it('should pass basic math', () => {
+    expect(1 + 1).to.equal(2);
+  });
+});
